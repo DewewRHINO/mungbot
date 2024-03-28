e07b83da6810133a5383466f5a29bdba6da95bc7
DewewRHINO <jrjayme@cpp.edu>
2024-03-28 16:28:26 -0700

    Wokring PoC, uses MySQL though, so cannot inject. Can interact with database though

--- mungbot.py
+++ mungbot.py
@@ -4,23 +4,12 @@ from sqlalchemy.orm import Session
 from database import SessionLocal
 import models
 import json
-
-# OpenAI API key setup
-openai.api_key = st.secrets["OPENAI_API_KEY"]
-
-# Custom image for the app icon and the assistant's avatar
-company_logo = 'https://media.discordapp.net/attachments/776228323960553494/1220579653048336514/chefmarshall.png?ex=660f7462&is=65fcff62&hm=525117ff881f1853ac95eb1ba90a412fa70921f8b732f2917445bbba8adbd462&=&format=webp&quality=lossless&width=671&height=671'
-
-# Configure streamlit page
-st.image(company_logo, width=100)  # Display the company logo at the top
-st.title("MungBot - Italian Enthusiast")
-
-# Function to fetch information about food items from the database
+import time
 
 def get_food_info(food):
-    print(f"This is the parameter that was put into the function: {food}")
+    # print(f"This is the parameter that was put into the function: {food}")
     db = SessionLocal()
-    print(db.query(models.FoodItem).filter(models.FoodItem.name.ilike(f"%{food}%")).first())
+    # print(db.query(models.FoodItem).filter(models.FoodItem.name.ilike(f"%{food}%")).first())
     try:
         # Change contains to ilike for case-insensitive search
         food_item = db.query(models.FoodItem).filter(
@@ -54,39 +43,76 @@ def update_food_info(food, price):
         return f"An error occurred: {str(e)}"
     finally:
         db.close()
+        
+def write_message(message):
+    full_response = ""
+    # Simulate stream of response with milliseconds delay
+    for chunk in message:
+        full_response += chunk + ""
+        time.sleep(0.03)
+        # Add a blinking cursor to simulate typing
+        message_placeholder.markdown(full_response + "▌")
+    
+    message_placeholder.markdown(full_response)
 
+# Custom image for the app icon and the assistant's avatar
+company_logo = 'https://media.discordapp.net/attachments/776228323960553494/1220579653048336514/chefmarshall.png?ex=660f7462&is=65fcff62&hm=525117ff881f1853ac95eb1ba90a412fa70921f8b732f2917445bbba8adbd462&=&format=webp&quality=lossless&width=671&height=671'
+gif_url = "https://media.discordapp.net/attachments/1202751965088579634/1221297791024894023/IMG_0132.gif?ex=66121133&is=65ff9c33&hm=a4b9391199a99d7b8dd87472476905135fa9a5a3821e80e7dc9600777d4becec&=&width=225&height=300"  # Example GIF URL
+
+# Configure streamlit page
+st.set_page_config(
+    page_title="MungBot",
+    page_icon=company_logo
+)
 
 # Initialize chat history if not already initialized
 if 'messages' not in st.session_state:
-    st.session_state['messages'] = [{"role": "system",
-                                     "content": "Hello! I am Marshall. I am here to answer any questions you may have about our Italian Restaurant."}]
+    st.session_state['messages'] = [{"role": "system", 
+                                     "content": "Hello! I Mung, I am here to answer any of your questions about the SWIFT Itallian Restauraunt."}]
 
-# Display chat messages from history
+# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     if message["role"] == 'assistant':
-        with st.container():
-            st.image(company_logo, width=50)  # Adjust size as needed
-            st.write(message["content"])
+        with st.chat_message(message["role"], avatar=company_logo):
+            st.markdown(message["content"])
     else:
-        with st.container():
-            st.write(f"You: {message['content']}")
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
 # Chat logic
-query = st.text_input("Ask me anything about our food items:", key="query")
-
-if st.button("Send"):
+if query := st.chat_input("Ask me anything"):
     
-    if query:  
-        # Add user message to chat history
-        # st.session_state.messages is the messages equivalent
-        st.session_state.messages.append({"role": "user", "content": query})
-                
-        st.session_state.messages = [message for message in st.session_state.messages if message["role"] not in ["tool"]]
+    # Add user message to chat history
+    # st.session_state.messages.append({"role": "user", "content": query})
+    message = {"role": "user", "content": query}
+    
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(query)
+        
+    # Check for specific keywords in the query
+    sensitive_keywords = ["admin", "phone number", "password", "minh"]
+    if any(keyword in query.lower() for keyword in sensitive_keywords):
+        # Display the GIF without sending the query to the chain
+        with st.chat_message("assistant", avatar=company_logo):
+            st.image(gif_url, caption="Oops, let's not talk about that here!")
+            
+        # Add a generic assistant message to chat history to maintain flow
+        st.session_state.messages.append({"role": "assistant", "content": "Oops, let's not talk about that here!"})
+    else:
+        # Process query normally if no sensitive keywords are found
+        with st.chat_message("assistant", avatar=company_logo):
+            message_placeholder = st.empty()
+            # Send user's question to our chain
+            # result = st.session_state['chain']({"question": query})
+            # response = result['answer']
+            st.session_state.messages.append({"role": "user", "content": query})
+            messages = [{"role": "user", "content": query}]
 
         # OpenAI call to process the input
         response = openai.chat.completions.create(
             model="gpt-3.5-turbo",
-            messages=st.session_state.messages,
+            messages=messages,
             tools=[
                 {
                     "type": "function",
@@ -134,7 +160,7 @@ if st.button("Send"):
 
         response_message = response.choices[0].message
         tool_calls = response_message.tool_calls
-        print(f"This is the tool calls: {tool_calls}")
+        # print(f"This is the tool calls: {tool_calls}")
 
         # If the AI determines a tool should be used, call the get_food_info function
         # Example tool usage detection, replace with actual logic based on AI response
@@ -152,11 +178,13 @@ if st.button("Send"):
                 function_response = function_to_call(
                     food=function_args.get("food_item"),
                 )
+                write_message(function_response)
             else: 
                 function_response = function_to_call(
                 food=function_args.get("food_item"),
                 price=function_args.get("price")
-            )
+                )
+                write_message(function_response)
 
             st.session_state.messages.append(
                 {
@@ -165,13 +193,7 @@ if st.button("Send"):
                     "name": function_name,
                     "content": function_response,
                 }
-            )  # extend conversation with function response
-
-# Display updated chat messages
-for message in st.session_state.messages:
-    with st.container():
-        if message["role"] == 'assistant':
-            st.image(company_logo, width=50)
-            st.write(message["content"])
-        else:
-            st.write(f"You: {message['content']}")
+            ) 
+            
+            # Add assistant message to chat history
+            # st.session_state.messages.append({"role": "assistant", "content": response})
